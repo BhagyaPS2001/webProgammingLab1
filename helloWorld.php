6ac5915dd92da6a2cf112ed60353c2aa31a1e9be
bhagya <22mca01@mgits.ac.in>
2023-01-24 14:10:01 +0530

    Initial

---
+++ helloWorld.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>My first program</title>
+</head>
+<body>
+<h2><center>My first program </center></h2>
+<?php
+echo 'Hello World';
+?>
+</body>
+</html>
---
+++ sampl1.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+  <title>Sample 1</title>
+</head>
+<body>
+  <?php
+  $txt='cats';
+  echo 'I love '.$txt; ?>
+</body>
+</html>
